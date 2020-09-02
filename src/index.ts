9c2a8a259d6602cff901a474d93920ce45fe09eb
cecilpert <cecile.hilpert@gmail.com>
2020-09-02 14:43:13 +0200

    reject promise when no file is found

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { fork } from "child_process";
 export type SearchFn = (...searchParam: string[]) => Promise<any>;
 
+
+
 export async function SearchFn(...searchParam: string[]) {
     return new Promise ( (resolve, reject)=>{
         let ChildProcess = fork(`${__dirname}/lib/glob.js`, [...searchParam]);
         ChildProcess.on('message', (d:{}) => {
             if('type' in d)
-                if(d['type'] === 'results')
-                    resolve(d['data']);
+                if(d["type"] === 'results'){
+                    let empty = true; 
+                    const data: string[][] = d["data"]
+                    data.map(path_results => {if (path_results.length !== 0)empty=false }) //check if we found results
+                    if (empty) reject("not found")
+                    else resolve(data)
+                }
+
             //console.log('Found w/ data: ', d);
         })
     });
